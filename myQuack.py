208c503d6a6699cd98839772f68105f1c3ec348b
mcafun <maurice.cafun@live.com>
2017-05-09 20:48:23 +1000

    Initial Files and Preprocessing (not fully sure)
    Added the two files from blackboard and had an attempt at the
    Preprocessing part. Am not sure if X is correct but y is on point

---
+++ myQuack.py
@@ -0,0 +1,150 @@
+
+'''
+
+Some partially defined functions for the Machine Learning assignment. 
+
+You should complete the provided functions and add more functions and classes as necessary.
+ 
+Write a main function that calls different functions to perform the required tasks.
+
+'''
+
+import numpy as np
+import os
+import csv
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+def my_team():
+    '''
+    Return the list of the team members of this assignment submission as a list
+    of triplet of the form (student_number, first_name, last_name)
+    
+    '''
+    return [ (9708651, 'Christopher', 'O\'Rafferty'), (9400001, 'Moira', 'Quinn'), (7226209, 'Maurice', 'Cafun') ]
+    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+def prepare_dataset(dataset_path):
+    '''  
+    Read a comma separated text file where 
+	- the first field is a ID number 
+	- the second field is a class label 'B' or 'M'
+	- the remaining fields are real-valued
+
+    Return two numpy arrays X and y where 
+	- X is two dimensional. X[i,:] is the ith example
+	- y is one dimensional. y[i] is the class label of X[i,:]
+          y[i] should be set to 1 for 'M', and 0 for 'B'
+
+    @param dataset_path: full path of the dataset text file
+
+    @return
+	X,y
+    '''
+    i = 0
+    X = np.empty(shape=[0,2],dtype=str)
+    y = np.empty(shape=[0,1],dtype=bool)
+
+    # Read in Data with CSV Reader
+    with open(dataset_path, newline='') as csvfile:
+        patients = csv.reader(csvfile,delimiter=',')
+        for row in patients:
+            data = ', '.join(row)
+#            X = np.append(X, [[i, j]], axis=0)
+            X = np.append(X, [[i,data]], axis=0)
+            y = np.append(y, [[int(row[1] == 'M')]], axis=0)
+            i += 1
+                  
+    return X,y 
+
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def build_NB_classifier(X_training, y_training):
+    '''  
+    Build a Naive Bayes classifier based on the training set X_training, y_training.
+
+    @param 
+	X_training: X_training[i,:] is the ith example
+	y_training: y_training[i] is the class label of X_training[i,:]
+
+    @return
+	clf : the classifier built in this function
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def build_DT_classifier(X_training, y_training):
+    '''  
+    Build a Decision Tree classifier based on the training set X_training, y_training.
+
+    @param 
+	X_training: X_training[i,:] is the ith example
+	y_training: y_training[i] is the class label of X_training[i,:]
+
+    @return
+	clf : the classifier built in this function
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def build_NN_classifier(X_training, y_training):
+    '''  
+    Build a Nearrest Neighbours classifier based on the training set X_training, y_training.
+
+    @param 
+	X_training: X_training[i,:] is the ith example
+	y_training: y_training[i] is the class label of X_training[i,:]
+
+    @return
+	clf : the classifier built in this function
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def build_SVM_classifier(X_training, y_training):
+    '''  
+    Build a Support Vector Machine classifier based on the training set X_training, y_training.
+
+    @param 
+	X_training: X_training[i,:] is the ith example
+	y_training: y_training[i] is the class label of X_training[i,:]
+
+    @return
+	clf : the classifier built in this function
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if __name__ == "__main__":
+    pass
+    # call your functions here
+
+    # my_team
+    print(my_team())
+    
+    # prepare_dataset
+    script_path = os.path.abspath(__file__)
+    script_dir = os.path.split(script_path)[0]
+    rel_path = "medical_records.data"
+    abs_file_path = os.path.join(script_dir, rel_path)
+    print(prepare_dataset(abs_file_path))
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+
+
